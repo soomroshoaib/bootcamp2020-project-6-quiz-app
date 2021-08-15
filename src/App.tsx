27f396731018d2b31d6593b9500e1b0559cc7bdf
soomroshoaib <soomroshoaib178@gmail.com>
2021-08-14 21:05:28 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import {getquzDetails} from "./services/Quiz-servics"
+import { Questiontype} from "./services/type/quiz-type";
+import QuestionCard from './component/QuestionCard';
+
 import './App.css';
 
 function App() {
+  const [quiz, setquiz] = useState<Questiontype[]>([])
+  const [step, setstep] = useState(0)
+  const [Score, setScore] = useState(0)
+  
+  
+  useEffect(()=>{
+    async function fatchgit() {
+      const question:Questiontype[] = await getquzDetails(5, 'easy')
+      //console.log(question)
+      setquiz(question)
+    }
+    fatchgit();
+  },[])
+  const handleSubmit=(e: React.FormEvent<EventTarget>, userAns:string)=>{
+     
+      e.preventDefault();
+      //console.log(userAns)
+      
+
+
+      const currentQestion:Questiontype = quiz[step];
+      console.log("correct And:"+ currentQestion.correct_answer+ "--user Selection "+ userAns )
+      if(userAns === currentQestion.correct_answer){
+        setScore(Score + 1)
+      }
+      if(step !== quiz.length - 1){
+        setstep(step + 1);
+
+      }else{
+        alert("your Final Score is  "+ Score+ "out of:"+ quiz.length)
+        setstep(0)
+        setScore(0)
+      }
+      
+  }
+  if(!quiz?.length){
+    return <h1>lloading ...</h1>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{}}>Quiz App</h1>
+      <QuestionCard 
+
+  
+       options={quiz[step].option}
+       question={quiz[step].question}
+       callback={handleSubmit}
+
+      
+      />
     </div>
   );
 }
